d6259f9fde202b27cdadd8129bc8dabb8b91042d
Hassan Abbasi <hassan.abbp@gmail.com>
2020-12-12 18:46:47 +0330

    Refactor day12.py

--- 2020/Day12/day12.py
+++ 2020/Day12/day12.py
@@ -2,14 +2,10 @@ class Ship:
     def __init__(self):
         self.face = 0
         self.x = self.y = 0
-        self.d = {0: self.e, 90: self.s, 180: self.w, 270: self.n}
+        self.d = {0: self.e, 90: self.n, 180: self.e, 270: self.n}
 
     def fd(self, num):
-        self.d[self.face](num)
-
-    def lt(self, deg):
-        self.face -= deg
-        self.face %= 360
+        self.d[self.face](num if self.face in (0, 270) else -num)
 
     def rt(self, deg):
         self.face += deg
@@ -18,14 +14,8 @@ class Ship:
     def n(self, num):
         self.y += num
 
-    def s(self, num):
-        self.y -= num
-
     def e(self, num):
         self.x += num
-
-    def w(self, num):
-        self.x -= num
 
     def man_dis(self):
         return abs(self.x) + abs(self.y)
@@ -40,54 +30,42 @@ class Ship2:
         self.x += self.wp[0] * num
         self.y += self.wp[1] * num
 
-    def lt(self, deg):
-        self.rt(-deg % 360)
-
     def rt(self, deg):
+        deg %= 360
         for _ in range(deg // 90):
             self.wp[0], self.wp[1] = self.wp[1], -self.wp[0]
 
     def n(self, num):
         self.wp[1] += num
 
-    def s(self, num):
-        self.wp[1] -= num
-
     def e(self, num):
         self.wp[0] += num
-
-    def w(self, num):
-        self.wp[0] -= num
 
     def man_dis(self):
         return abs(self.x) + abs(self.y)
 
 
-ship = Ship()
-ship2 = Ship2()
-for cmd in open('inp12').readlines():
-    c = cmd[0]
-    num = int(cmd[1:])
-    if c == 'N':
-        ship.n(num)
-        ship2.n(num)
-    elif c == 'S':
-        ship.s(num)
-        ship2.s(num)
-    elif c == 'E':
-        ship.e(num)
-        ship2.e(num)
-    elif c == 'W':
-        ship.w(num)
-        ship2.w(num)
-    elif c == 'L':
-        ship.lt(num)
-        ship2.lt(num)
-    elif c == 'R':
-        ship.rt(num)
-        ship2.rt(num)
-    else:
-        ship.fd(num)
-        ship2.fd(num)
-print(ship.man_dis())
-print(ship2.man_dis())
+def run(ship, cmds):
+    for cmd in cmds:
+        c = cmd[0]
+        num = int(cmd[1:])
+        if c == 'N':
+            ship.n(num)
+        elif c == 'S':
+            ship.n(-num)
+        elif c == 'E':
+            ship.e(num)
+        elif c == 'W':
+            ship.e(-num)
+        elif c == 'R':
+            ship.rt(num)
+        elif c == 'L':
+            ship.rt(-num)
+        else:
+            ship.fd(num)
+    return ship.man_dis()
+
+
+cmds = open('inp12').readlines()
+print(run(Ship(), cmds))
+print(run(Ship2(), cmds))
