d5299cdde6107be319374768a781c7d59b5b1545
Hassan Abbasi <hassan.abbp@gmail.com>
2020-12-12 19:04:52 +0330

    Play with day12.py !

--- 2020/Day12/day12.py
+++ 2020/Day12/day12.py
@@ -4,10 +4,10 @@ class Ship:
         self.x = self.y = 0
         self.d = {0: self.e, 90: self.n, 180: self.e, 270: self.n}
 
-    def fd(self, num):
+    def f(self, num):
         self.d[self.face](num if self.face in (0, 270) else -num)
 
-    def rt(self, deg):
+    def r(self, deg):
         self.face += deg
         self.face %= 360
 
@@ -17,22 +17,18 @@ class Ship:
     def e(self, num):
         self.x += num
 
-    def man_dis(self):
-        return abs(self.x) + abs(self.y)
-
 
 class Ship2:
     def __init__(self):
         self.wp = [10, 1]
         self.x = self.y = 0
 
-    def fd(self, num):
+    def f(self, num):
         self.x += self.wp[0] * num
         self.y += self.wp[1] * num
 
-    def rt(self, deg):
-        deg %= 360
-        for _ in range(deg // 90):
+    def r(self, deg):
+        for _ in range(deg % 360 // 90):
             self.wp[0], self.wp[1] = self.wp[1], -self.wp[0]
 
     def n(self, num):
@@ -41,31 +37,14 @@ class Ship2:
     def e(self, num):
         self.wp[0] += num
 
-    def man_dis(self):
-        return abs(self.x) + abs(self.y)
-
 
 def run(ship, cmds):
     for cmd in cmds:
-        c = cmd[0]
+        c = cmd[0].lower()
         num = int(cmd[1:])
-        if c == 'N':
-            ship.n(num)
-        elif c == 'S':
-            ship.n(-num)
-        elif c == 'E':
-            ship.e(num)
-        elif c == 'W':
-            ship.e(-num)
-        elif c == 'R':
-            ship.rt(num)
-        elif c == 'L':
-            ship.rt(-num)
-        else:
-            ship.fd(num)
-    return ship.man_dis()
+        ship.__getattribute__(c)(num)
+    return abs(ship.x) + abs(ship.y)
 
 
-cmds = open('inp12').readlines()
-print(run(Ship(), cmds))
-print(run(Ship2(), cmds))
+cmds = open('inp12').read().replace('L', 'R-').replace('S', 'N-').replace('W', 'E-').split()
+print(run(Ship(), cmds), run(Ship2(), cmds), sep='\n')
