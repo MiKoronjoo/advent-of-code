7f7b470401aebbb091b7d68da52415025daafdca
Hassan Abbasi <hassan.abbp@gmail.com>
2020-12-15 14:16:03 +0330

    Fix a bug in day15.py | Refactor day15.py

--- 2020/Day15/day15.py
+++ 2020/Day15/day15.py
@@ -1,13 +1,11 @@
 lst = list(map(int, input().split(',')))
-mem = {x: i for i, x in enumerate(lst)}
+mem = {x: i for i, x in enumerate(lst[:-1])}
 while len(lst) != 30000000:
-    if len(lst) == 2020:
-        print(lst[-1])
-    n = lst[-1]
-    i = mem.get(n)
-    mem[n] = len(lst) - 1
+    last = lst[-1]
+    i = mem.get(last)
+    mem[last] = len(lst) - 1
     if i is None:
         lst.append(0)
     else:
         lst.append(len(lst) - 1 - i)
-print(lst[-1])
+print(lst[2019], lst[-1], sep='\n')
