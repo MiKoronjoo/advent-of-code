1cd213225aeda54fed0770af13f07f7d8ef4e406
Hassan Abbasi <hassan.abbp@gmail.com>
2020-12-17 00:53:20 +0330

    Refactor day16.py

--- 2020/Day16/day16.py
+++ 2020/Day16/day16.py
@@ -1,9 +1,8 @@
 a, b, c = open('inp16').read().strip().split('\n\n')
 ranges = {}
 for r in a.split('\n'):
-    name, rem = r.split(': ')
-    r1, r2 = rem.split(' or ')
-    ranges[name] = tuple(map(int, r1.split('-'))) + tuple(map(int, r2.split('-')))
+    name, rng = r.replace(' or ', '-').split(': ')
+    ranges[name] = tuple(map(int, rng.split('-')))
 my_tic = list(map(int, b.split('\n')[1].split(',')))
 nr_tic = [list(map(int, l.split(','))) for l in c.split('\n')[1:]]
 res = []
@@ -22,13 +21,9 @@ for x in res:
 nr_tic.append(my_tic)
 d = {}
 for i in range(len(my_tic)):
-    d[i] = []
-    for name, r in ranges.items():
-        for num in [t[i] for t in nr_tic]:
-            if not (r[0] <= num <= r[1] or r[2] <= num <= r[3]):
-                break
-        else:
-            d[i].append(name)
+    column = [t[i] for t in nr_tic]
+    d[i] = [name for name, r in ranges.items()
+            if next((0 for num in column if not (r[0] <= num <= r[1] or r[2] <= num <= r[3])), 1)]
 res2 = 1
 while d:
     k, v = min(d.items(), key=lambda x: len(x[1]))
