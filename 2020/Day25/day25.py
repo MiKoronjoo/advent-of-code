c950704b5870b370a666f5670dbc506826ad2042
Hassan Abbasi <hassan.abbp@gmail.com>
2020-12-26 01:40:57 +0330

    It lied to me! There was only one part for Day25

--- 2020/Day25/day25.py
+++ 2020/Day25/day25.py
@@ -1,24 +1,13 @@
-def loop(goal, sn=7):
-    i = 0
-    n = 1
-    while n != goal:
-        i += 1
-        n *= sn
-        n %= 20201227
-    return i
-
-
-def loop2(sn, ls):
-    n = 1
-    for _ in range(ls):
-        n *= sn
-        n %= 20201227
-    return n
-
-
 card_pk = int(input())
 door_pk = int(input())
-cls = loop(card_pk)
-dls = loop(door_pk)
-print(loop2(door_pk, cls))
-print(loop2(card_pk, dls))
+i = 0
+n = 1
+while n != door_pk:
+    i += 1
+    n *= 7
+    n %= 20201227
+n = 1
+for _ in range(i):
+    n *= card_pk
+    n %= 20201227
+print(n)
