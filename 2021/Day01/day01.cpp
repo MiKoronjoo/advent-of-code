2d8ccb0bb82555301284ea04917399e55d6d5548
Hassan Abbasi <hassan.abbp@gmail.com>
2021-12-02 01:32:01 +0330

    Simplify day01@2021

--- 2021/Day01/day01.cpp
+++ 2021/Day01/day01.cpp
@@ -9,7 +9,7 @@ int main() {
         nums.push_back(temp);
     for (int i = 0; i < nums.size() - 1; i++) {
         ans1 += nums[i] < nums[i + 1];
-        ans2 += (i + 3 < nums.size()) and (nums[i] + nums[i + 1] + nums[i + 2] < nums[i + 1] + nums[i + 2] + nums[i + 3]);
+        ans2 += i + 3 < nums.size() and nums[i] < nums[i + 3];
     }
     std::cout << ans1 << '\n' << ans2 << '\n';
 }
--- 2021/Day01/day01.py
+++ 2021/Day01/day01.py
@@ -4,7 +4,7 @@ nums = list(map(int, sys.stdin.read().split()))
 ans1 = ans2 = 0
 for i in range(len(nums) - 1):
     ans1 += nums[i] < nums[i + 1]
-    ans2 += sum(nums[i:i + 3]) < sum(nums[i + 1:i + 4])
+    ans2 += i + 3 < len(nums) and nums[i] < nums[i + 3]
 
 print(ans1)
 print(ans2)
