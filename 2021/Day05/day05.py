967ef04bf301feb398c7d01bc043a5ab53aeb07d
Hassan Abbasi <hassan.abbp@gmail.com>
2021-12-06 00:48:48 +0330

    Add python code to Day05@2021

---
+++ 2021/Day05/day05.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+
+
+def line(x1, y1, x2, y2):
+    if x1 == x2:
+        if y1 > y2:
+            y1, y2 = y2, y1
+        for y in range(y1, y2 + 1):
+            covers[(x1, y)] += 1
+    elif y1 == y2:
+        if x1 > x2:
+            x1, x2 = x2, x1
+        for x in range(x1, x2 + 1):
+            covers[(x, y1)] += 1
+    elif abs(x1 - x2) == abs(y1 - y2):
+        if x1 > x2:
+            x1, x2, y1, y2 = x2, x1, y2, y1
+        step = 1 if y1 < y2 else -1
+        x = x1
+        for y in range(y1, y2 + step, step):
+            covers[(x, y)] += 1
+            x += 1
+
+
+covers = defaultdict(int)
+while True:
+    try:
+        a, b = input().split(' -> ')
+    except EOFError:
+        break
+    x1, y1 = map(int, a.split(','))
+    x2, y2 = map(int, b.split(','))
+    line(x1, y1, x2, y2)
+
+print(sum(1 for c in covers.values() if c > 1))
