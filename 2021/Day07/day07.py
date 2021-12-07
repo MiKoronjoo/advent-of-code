be624eee8e0d741e49c354999f6d1232e21fc55a
Hassan Abbasi <hassan.abbp@gmail.com>
2021-12-08 00:35:55 +0330

    Change day07.py

--- 2021/Day07/day07.py
+++ 2021/Day07/day07.py
@@ -1,5 +1,5 @@
 nums = list(map(int, input().split(',')))
 ans = min(sum(abs(n - i) for n in nums) for i in range(max(nums)))
 print(ans)
-ans2 = min(sum(abs((n - i) * (abs(n - i) + 1) // 2) for n in nums) for i in range(max(nums)))
+ans2 = min(sum(abs(n - i) * (abs(n - i) + 1) // 2 for n in nums) for i in range(max(nums)))
 print(ans2)
